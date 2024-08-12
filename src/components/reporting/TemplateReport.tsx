0717b69c569a45c7b945fd27733c5a1f64a657a8
danprasetyo <danprasetyoo21@gmail.com>
2024-08-12 09:20:37 +0700

    init

--- src/components/reporting/TemplateReport.tsx
+++ src/components/reporting/TemplateReport.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
-import { Grid, GridItem, Box, Text } from '@chakra-ui/react';
+import { Grid, GridItem, Button } from '@chakra-ui/react';
+import { Link } from 'react-router-dom';
 import RiuReport from './Riu/RiuReport';
 import SwissReport from './Swiss/SwissReport';
 
@@ -15,6 +16,30 @@ const TemplateReport: React.FC = () => {
       <GridItem>
         <SwissReport />
       </GridItem>
+      <Link to="/report">
+        <Button
+          color="white"
+          bg="blue.300"
+          fontFamily="Arial, sans-serif"
+          fontSize="16px"
+          borderRadius="md"
+          boxShadow="md"
+          _hover={{
+            bg: 'blue.500',
+            boxShadow: 'lg',
+          }}
+          _active={{
+            bg: 'blue.700',
+            boxShadow: 'none',
+          }}
+          _focus={{
+            boxShadow: 'outline',
+          }}
+          transition="all 0.2s ease-in-out"
+        >
+          Submit
+        </Button>
+      </Link>
     </Grid>
   );
 };
